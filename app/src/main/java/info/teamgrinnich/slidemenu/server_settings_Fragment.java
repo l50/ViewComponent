b11db4d2c5b62127f1c237d1a9a3ebf512c63b23
Jayson Grace <jayson.e.grace@gmail.com>
2015-02-28 15:16:18 -0700

    Initial Slide Menu
    Basic slide menu created, although it doesn't do anything super interesting yet.

---
+++ app/src/main/java/info/teamgrinnich/slidemenu/server_settings_Fragment.java
@@ -0,0 +1,23 @@
+package info.teamgrinnich.slidemenu;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by l on 2/28/15.
+ */
+public class server_settings_Fragment extends Fragment
+{
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        rootview = inflater.inflate(R.layout.server_settings_layout, container, false);
+        return rootview;
+    }
+}
