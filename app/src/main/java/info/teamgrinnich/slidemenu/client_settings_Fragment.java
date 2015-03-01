7a23e02835533a6474b0a2bc6f3cf5529ec60dca
Jayson Grace <jayson.e.grace@gmail.com>
2015-02-28 21:38:44 -0700

    Experimental version of ViewComponent added to experimental branch

--- app/src/main/java/info/teamgrinnich/slidemenu/client_settings_Fragment.java
+++ app/src/main/java/info/teamgrinnich/slidemenu/client_settings_Fragment.java
@@ -3,14 +3,14 @@ package info.teamgrinnich.slidemenu;
 /**
  * Created by l on 2/28/15.
  */
-//public class server_settings_Fragment extends Fragment
+//public class client_settings_Fragment extends Fragment
 //{
 //    View rootview;
 //    @Nullable
 //    @Override
 //    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 //    {
-//        rootview = inflater.inflate(R.layout.server_settings_layout, container, false);
+//        rootview = inflater.inflate(R.layout.client_settings_layout, container, false);
 //        return rootview;
 //    }
 //}
